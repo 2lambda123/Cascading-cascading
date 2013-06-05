e339aeddddf921639ae9c16a5a3e1eb4dcb28652
fderose <frank.derose1@gmail.com>
2013-06-05 13:42:04 -0600

    Update BaseFlow.java

--- cascading-core/src/main/java/cascading/flow/BaseFlow.java
+++ cascading-core/src/main/java/cascading/flow/BaseFlow.java
@@ -156,7 +156,6 @@ public abstract class BaseFlow<Config> implements Flow<Config>
     this.name = name;
     addSessionProperties( properties );
     initConfig( properties, defaultConfig );
-    initFromProperties( properties );
 
     this.flowStats = createPrepareFlowStats(); // must be last
     }
